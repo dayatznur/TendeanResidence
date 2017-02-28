609918aa7afea2fa18e4ec7bc79f80e79bb4e475
dayatznur <kepeer.groove@gmail.com>
2017-02-28 11:59:36 +0700

    penambahan icon ico

--- application/views/home_v.php
+++ application/views/home_v.php
@@ -4,6 +4,7 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE-edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="assets/img/tendean.ico" type=image/x-icon rel="shortcut icon">
 	
 	<title>Tendean Residences</title>
 
